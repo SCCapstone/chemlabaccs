0f70efd3a78b5d8d62320dae4d4db717ed4bab95
Michael Scribellito <mscribellito@gmail.com>
2013-11-10 20:43:59 -0500

    updated database

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 10, 2013 at 06:10 PM
+-- Generation Time: Nov 10, 2013 at 07:42 PM
 -- Server version: 5.5.33-31.1
 -- PHP Version: 5.3.17
 
@@ -37,14 +37,15 @@ CREATE TABLE IF NOT EXISTS `accidents` (
   `root` text COLLATE utf8_unicode_ci NOT NULL,
   `prevention` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `accidents`
 --
 
 INSERT INTO `accidents` (`id`, `date`, `time`, `building`, `room`, `description`, `severity`, `root`, `prevention`) VALUES
-(1, '11-13-2013', '12:30 am', 'a', 'b', 'c', 'medium', 'd', 'e');
+(1, '11-13-2013', '12:30 am', 'a', 'b', 'c', 'medium', 'd', 'e'),
+(2, '11-12-2013', '12:15 am', 'asdf', 'qqqttt', 'ttt', 'low', 'rrr', 'zzz');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('b97a93efb0ccf5083d3a4d269fa07325', '173.189.14.55', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36', 1384128407, 'a:4:{s:9:"user_data";s:0:"";s:18:"AUTH_authenticated";b:1;s:12:"AUTH_user_id";i:1;s:14:"AUTH_user_name";s:8:"scribell";}');
+('7dfdfce65d90ac043ecac01f94e96fa1', '173.189.14.55', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36', 1384134015, 'a:4:{s:9:"user_data";s:0:"";s:18:"AUTH_authenticated";b:1;s:12:"AUTH_user_id";i:1;s:14:"AUTH_user_name";s:8:"scribell";}');
 
 -- --------------------------------------------------------
 
@@ -81,14 +82,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password_hash` varchar(40) NOT NULL,
   `salt` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password_hash`, `salt`) VALUES
-(1, 'scribell@email.sc.edu', '314ad9e71da8e7406c3087c755af0cd17c62914d', '9812fe5cf3626343d6d43028ff410de1');
+(1, 'scribell@email.sc.edu', '314ad9e71da8e7406c3087c755af0cd17c62914d', '9812fe5cf3626343d6d43028ff410de1'),
+(2, 'cieplows@email.sc.edu', 'edc9508dc94891f6330eff41a45ab2187c86bd45', 'fe20263efe301dbd49f7b67847f35abe'),
+(3, 'carrow@email.sc.edu', '3c4273a93114c1c86f26a52a8c42ce4133e732b2', '495fbc11fe23b02f2b823576e2c7622b'),
+(4, 'hamodm@email.sc.edu', 'eaf0de3cceaad081088117abb732e5efa1c953f0', '604abb3a2c17f2430e0a9502d74cc6cc');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
