e9ecba1c6839426db9704239612f15a13e0e9ae7
timciep <cieplows@email.sc.edu>
2014-01-30 22:11:48 -0500

    Some Comments

--- application/controllers/charts.php
+++ application/controllers/charts.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+// Using Highcharts
+
 class Charts extends CI_Controller {
 
 	function Charts()
--- application/views/view_charts.php
+++ application/views/view_charts.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!-- Using Highcharts -->
 <html lang="en">
 <head>
 	<meta charset="utf-8">
