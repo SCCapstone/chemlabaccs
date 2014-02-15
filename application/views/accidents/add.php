7df0cc94b3c51498b6b629a1a5e9ba5e692bb7ec
cooperd2 <cooperd2@email.sc.edu>
2014-02-14 20:34:36 -0500

    Photo Gallery Additions

--- application/views/accidents/add.php
+++ application/views/accidents/add.php
@@ -1,5 +1,4 @@
 <?php
-
 $date = array(
     "id" => "date",
     "name" => "date",
@@ -59,10 +58,192 @@ $prevention = array(
     "rows" => 3,
     "value" => set_value("prevention")
 );
-
 ?>
+<!------------------------------------------------------------------------------------------------->
+<script>
+    function PreviewImage() {
+         // This function creates a dynamic preview of images that are present in a files input buffer.
+         // This function depends on Bootstrap 3.0 for styling of the thumbnail images. 
+         // Created by Demetrious Cooper 2/12/2014 for Chemlabaccs USC Capstone Project
+         
+         // Initialize a variable "container" which contains the image preview area.
+         var container = document.getElementById("preview_area");
+         // ubound is the maximum amount of photos allowed to be upload. 
+         // This bound can be set dynamically by using the function definition 
+         // function PreviewImage(size);
+      
+         var ubound = 10;
 
-<?php echo form_open("accidents/add/save", array("class" => "form-horizontal", "role" => "form")); ?>
+        /**************************************************************/
+        // Check to ensure that the length is less than of equal to our upper bound.
+     
+         var size = document.getElementById('filefield');
+         if(size.files.length>ubound)
+           {
+           // If the link doesn't satisfy the bound constraints, then alert the user.
+           alert("You May Select only 10 photos.");
+           
+           return false;
+           
+           }
+       /**************************************************************/
+       
+        else  
+        
+        // we have satisfied the condtion ["size"]<=["upper bound"]
+        //var previewbox = document.getElementById('preview_area');
+        
+         // Hide our button to prevent interruption from the user
+         // Show our loader to show the user that the photos are preparing to show a preview
+                   $(".btn").hide(10);
+        $('#uploadbtn').hide(10);
+        $('#linear_loader').show(10);
+
+         
+       // previewbox.removeAttribute("style");
+         
+        // Remove the style attribute to show the preview area.  
+        // The preview area is set to hidden by default.
+        
+        container.removeAttribute("style");
+        
+        // Remove the inner HTML of our container to show the new 
+        // elements in the file input buffer. 
+        
+        container.innerHTML = "";
+        
+        
+        // Loop thru each element in the file input buffer and 
+        // create a new "img" element.
+        
+        for(var i = 0;i<size.files.length;i++)
+        {
+        
+        // Use the FileReader for html5 to read the contents of our input field. 
+        
+            var oFReader = new FileReader();
+        
+        // Reads the URL data for the input element. 
+            oFReader.readAsDataURL(document.getElementById("filefield").files[i]);
+                
+        // On each load, the function will construct a new preview image.    
+            oFReader.onload = function (oFREvent) {
+                var new_div = document.createElement('div');
+                new_div.setAttribute("id","dynamic_comment" );
+                new_div.setAttribute("style","max-width:150px; margin-left:5px; margin-top:5px;");
+                new_div.setAttribute("class","col-sm-4");
+
+                var new_img = document.createElement('img');
+                //new_img.setAttribute('id', 'img_preview');
+                new_img.setAttribute('class', 'constrained_preview img-thumbnail row');
+                //new_img.setAttribute('id',getElementById("filefield").files[i]);
+                new_img.src = oFREvent.target.result;
+                 
+                // document.getElementById("imgpreview").src = oFREvent.target.result;
+                var check = new_img.src;
+                check = check.substr(5,9);
+                
+              if(check=="image/jpe" || check=="image/png" || check=="image/gif" || check=="image/jpeg")
+            
+             /*{
+                container.appendChild(new_img);
+             }*/
+                {
+                    var description_field = document.createElement('textarea');
+                           description_field.setAttribute('placeholder',"Say Something!");
+                            description_field.setAttribute('name',"dymanic_comment[]");
+                            description_field.setAttribute('class',"row dynamic_comment");
+                            description_field.setAttribute('type',"text");
+
+
+                    
+                    new_div.appendChild(new_img);
+                    new_div.appendChild(description_field);
+                    container.appendChild(new_div);
+                }
+             
+           
+            };                      
+        }
+         
+         // Once the new image elements have been created, the upload button can be show again.
+         // The linear loader is hidden
+         //  again.  
+        $('#uploadbtn').show(10);
+        $('#linear_loader').hide(10);
+        $(".btn").show(10);
+   
+    };// EOF PreviewImage(); 
+</script>
+<!------------------------------------ Javascript for custom upload area -------------------------->
+<!------------------------------------------------------------------------------------------------->
+
+<style>
+    .formcs{
+            width: auto !important;
+            max-width: 980px;
+                    margin:0 auto;
+
+       
+        }
+    .constrained_preview{
+
+        max-width: 150px;
+        min-width: 150px;
+        max-height: 150px !important;
+        min-height: 150px !important;
+
+        margin-bottom:5px;
+
+    }
+    .dynamic_comment{
+        
+       width: auto;
+       min-width:150px;
+       max-width: 150px;
+       min-height:100px;
+       padding:5px;
+       max-height:200px !important;
+       font-size: 12px;
+       line-height:1.4em;
+       font-color:#333;
+       border-style: solid;
+       border-color: #DDD;
+       background-color: #f8f8f8;
+       border-width: 1px;
+       padding-bottom:5px;
+       
+        
+    }
+    textarea:focus
+{
+background-color:#fff;
+} 
+    #dymanic_comment{
+       max-height:200px !important;
+       width: 200px;
+       margin-left: 10px;
+        
+    }
+    #filefield{
+        visibility: none;
+
+    }
+    
+    .well{
+        overflow:auto !important;
+        padding:15px 12px;
+        width:auto;
+        min--width: 600px !important;
+        
+    }
+
+
+
+</style>
+
+<!------------------------------------------------------------------------------------------------->
+<?php echo form_open_multipart("accidents/add/save", array("class" => "form-horizontal formcs", "role" => "form","encytype" =>"multipart/form-data")); ?>
 
 <div class="form-group">
     <label for="<?php echo $date["id"]; ?>" class="col-sm-2 control-label"><?php echo $date["placeholder"]; ?></label>
@@ -109,22 +290,22 @@ $prevention = array(
     <label class="col-sm-2 control-label">Severity</label>
     <div class="col-sm-10">
         <?php echo form_error("severity"); ?>
-        <?php $i = 0; foreach(array("low", "medium", "high") as $severity): ?>
-        <?php
-        
-        $selected = "";
-        if ($this->input->post("severity") == $severity) {
-            $selected = 'checked="checked"';
-        }
-        
-        ?>
-        <div class="radio">
-            <label>
-                <input type="radio" name="severity" id="severity_<?php echo $severity; ?>" value="<?php echo $severity; ?>" <?php echo $selected; ?>>
-                <?php echo severity_scale($severity); ?>
-            </label>
-        </div>
-        <?php endforeach; ?>
+        <?php $i = 0;
+        foreach (array("low", "medium", "high") as $severity):
+            ?>
+            <?php
+            $selected = "";
+            if ($this->input->post("severity") == $severity) {
+                $selected = 'checked="checked"';
+            }
+            ?>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="severity" id="severity_<?php echo $severity; ?>" value="<?php echo $severity; ?>" <?php echo $selected; ?>>
+    <?php echo severity_scale($severity); ?>
+                </label>
+            </div>
+<?php endforeach; ?>
     </div>
 </div>
 
@@ -132,26 +313,53 @@ $prevention = array(
     <label for="<?php echo $root["id"]; ?>" class="col-sm-2 control-label"><?php echo $root["placeholder"]; ?></label>
     <div class="col-sm-10">
         <?php echo form_error($root["name"]); ?>
-        <?php echo form_textarea($root); ?>
+<?php echo form_textarea($root); ?>
         <span class="help-block"><?php echo lang('cla_f_root'); ?></span>
     </div>
 </div>
 
 <div class="form-group">
     <label for="<?php echo $prevention["id"]; ?>" class="col-sm-2 control-label"><?php echo $prevention["placeholder"]; ?></label>
+
     <div class="col-sm-10">
         <?php echo form_error($prevention["name"]); ?>
-        <?php echo form_textarea($prevention); ?>
+<?php echo form_textarea($prevention); ?>
         <span class="help-block"><?php echo lang('cla_f_prevention'); ?></span>
     </div>
 </div>
 
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-        <div class="well">
-        <?php echo form_button(array("type" => "submit", "class" => "btn btn-success", "content" => '<span class="glyphicon glyphicon-plus"></span> Add')); ?>
+
+        <!------------------------------------------------Desktop Preview Area------------------------------------------------------------->
+        <label>You may add up to 10 photos of the accident.</label>
+        <div class="well" id="preview_area" style="display:none;" >
+
         </div>
-    </div>
-</div>
+        <span style="visibility:hidden;"><input type="file" name="filefield[]" id="filefield"  onchange="PreviewImage();" multiple="" accept="image/*"></span>
+
+
+
+        <!------------------------------------------------------------------------------------------------------------------------>
+        <!-------------------Custom Upload Button-------------------------->
+        <div class="well well-sm">
+            <div id ="uploadbtn" class="btn btn-primary"><span class="glyphicon glyphicon-camera" type="" method=""></span> Add Photos</div><div id="linear_loader" style="display:none;"><img src="../img/linear_loader.GIF"/></div>
+            <!----------------------------------------------------------------->
+
+
+            <script>
+
+                document.querySelector('#uploadbtn').addEventListener('click', function(e) {
+                    var fileInput = document.querySelector('#filefield');
+                    //click(fileInput); // Simulate the click with a custom event.
+                    fileInput.click(); // Or, use the native click() of the file input.
+                }, false);
+
+            </script>
+
+        </div>
+        <span style="float: right;"> <?php echo form_button(array("type" => "submit", "class" => "btn btn-success", "content" => '<span class="glyphicon glyphicon-plus"></span> Add Report')); ?></span>
+
+
 
 <?php echo form_close(); ?>
--- application/views/accidents/detail.php
+++ application/views/accidents/detail.php
@@ -1,5 +1,6 @@
-<?php
 
+
+<?php
 $date = array(
     "id" => "date",
     "name" => "date",
@@ -58,51 +59,85 @@ $prevention = array(
     "rows" => 3,
     "value" => set_value("prevention", $details->prevention)
 );
-
 ?>
 
-<?php echo form_open("accidents/add/save", array("class" => "form-horizontal", "role" => "form")); ?>
+
+
+<style>
+
+    #photoview{
+        max-width: 815px;
+        margin:0 auto;
+        margin-bottom:50px;
+        height:auto;
+        width:auto !important;
+
+        padding-left:15px;
+    }
+    .equamargins{
+        width: 90% !important;
+    }
+    .formcs{
+        max-width:900px;
+        margin:0 auto;
+
+
+    }
+
+
+
+</style>
+<?php
+/*
+  $params = array($details->id);
+
+  $this->load->library('gallerybuilder',$params);
+ */
+?>
+
+
+<?php echo form_open("accidents/add/save", array("class" => "form-horizontal formcs hidden", "role" => "form")); ?>
 
 <?php echo form_hidden("revision_of", $details->revision_of); ?>
 <?php echo form_hidden("user", $details->user); ?>
 
-<div class="form-group">
+<div class="form-group ">
     <label for="<?php echo $date["id"]; ?>" class="col-sm-2 control-label"><?php echo $date["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($date["name"]); ?>
-        <?php echo form_input($date); ?>
+<?php echo form_error($date["name"]); ?>
+<?php echo form_input($date); ?>
     </div>
 </div>
 
 <div class="form-group">
     <label for="<?php echo $time["id"]; ?>" class="col-sm-2 control-label"><?php echo $time["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($time["name"]); ?>
-        <?php echo form_input($time); ?>
+<?php echo form_error($time["name"]); ?>
+<?php echo form_input($time); ?>
     </div>
 </div>
 
 <div class="form-group">
     <label for="<?php echo $building["id"]; ?>" class="col-sm-2 control-label"><?php echo $building["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($building["name"]); ?>
-        <?php echo form_dropdown($building["name"], get_buildings(), set_value(CI()->input->post($building["name"]), $details->building), 'class="form-control" id="building"'); ?>
+<?php echo form_error($building["name"]); ?>
+<?php echo form_dropdown($building["name"], get_buildings(), set_value(CI()->input->post($building["name"]), $details->building), 'class="form-control" id="building"'); ?>
     </div>
 </div>
 
 <div class="form-group">
     <label for="<?php echo $room["id"]; ?>" class="col-sm-2 control-label"><?php echo $room["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($room["name"]); ?>
-        <?php echo form_input($room); ?>
+<?php echo form_error($room["name"]); ?>
+<?php echo form_input($room); ?>
     </div>
 </div>
 
 <div class="form-group">
     <label for="<?php echo $description["id"]; ?>" class="col-sm-2 control-label"><?php echo $description["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($description["name"]); ?>
-        <?php echo form_textarea($description); ?>
+<?php echo form_error($description["name"]); ?>
+<?php echo form_textarea($description); ?>
         <span class="help-block"><?php echo lang('cla_f_description'); ?></span>
     </div>
 </div>
@@ -110,22 +145,21 @@ $prevention = array(
 <div class="form-group">
     <label class="col-sm-2 control-label">Severity</label>
     <div class="col-sm-10">
-        <?php echo form_error("severity"); ?>
-        <?php $i = 0; foreach(array("low", "medium", "high") as $severity): ?>
-        <?php
-        
-        $selected = "";
-        if ($this->input->post("severity") == $severity || $details->severity == $severity) {
-            $selected = 'checked="checked"';
-        }
-        
-        ?>
-        <div class="radio">
-            <label>
-                <input type="radio" name="severity" id="severity_<?php echo $severity; ?>" value="<?php echo $severity; ?>" <?php echo $selected; ?>>
-                <?php echo severity_scale($severity); ?>
-            </label>
-        </div>
+<?php echo form_error("severity"); ?>
+<?php $i = 0;
+foreach (array("low", "medium", "high") as $severity): ?>
+    <?php
+    $selected = "";
+    if ($this->input->post("severity") == $severity || $details->severity == $severity) {
+        $selected = 'checked="checked"';
+    }
+    ?>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="severity" id="severity_<?php echo $severity; ?>" value="<?php echo $severity; ?>" <?php echo $selected; ?>>
+            <?php echo severity_scale($severity); ?>
+                </label>
+            </div>
         <?php endforeach; ?>
     </div>
 </div>
@@ -134,7 +168,7 @@ $prevention = array(
     <label for="<?php echo $root["id"]; ?>" class="col-sm-2 control-label"><?php echo $root["placeholder"]; ?></label>
     <div class="col-sm-10">
         <?php echo form_error($root["name"]); ?>
-        <?php echo form_textarea($root); ?>
+<?php echo form_textarea($root); ?>
         <span class="help-block"><?php echo lang('cla_f_root'); ?></span>
     </div>
 </div>
@@ -142,8 +176,8 @@ $prevention = array(
 <div class="form-group">
     <label for="<?php echo $prevention["id"]; ?>" class="col-sm-2 control-label"><?php echo $prevention["placeholder"]; ?></label>
     <div class="col-sm-10">
-        <?php echo form_error($prevention["name"]); ?>
-        <?php echo form_textarea($prevention); ?>
+<?php echo form_error($prevention["name"]); ?>
+<?php echo form_textarea($prevention); ?>
         <span class="help-block"><?php echo lang('cla_f_prevention'); ?></span>
     </div>
 </div>
@@ -151,9 +185,82 @@ $prevention = array(
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
         <div class="well">
-            <?php echo form_button(array("type" => "submit", "class" => "btn btn-success", "content" => '<span class="glyphicon glyphicon-pencil"></span> Save')); ?>
+<?php echo form_button(array("type" => "submit", "class" => "btn btn-success", "content" => '<span class="glyphicon glyphicon-pencil"></span> Save')); ?>
         </div>
     </div>
 </div>
 
+
+
+*/
+<?php echo form_close(); ?>
+
+
+<!-----------------Demo---------------------->
+<div class ="container formcs">
+    <!-------Time Information ----->
+    <div class="panel panel-default" id = "time_info">
+        <div class="panel-heading"><h4>Time</h4></div>
+        <div class="panel-body">
+            <b> Date: </b> <?php echo $date['value']; ?>
+            <p></p>
+            <b>Time:</b> <?php echo $time['value']; ?>
+            <p></p>
+
+        </div>
+    </div>
+
+    <!-----Building Information--->
+
+    <div class="panel panel-default" id = "building_info">
+        <div class="panel-heading"><h4>Building Information</h4></div>
+        <div class="panel-body">
+            <b> Building Name:  </b> <?php echo $building['value']; ?>
+            <p></p>
+            <b>Room Number:</b> <?php echo $room['value']; ?>
+            <p></p>
+        </div>
+    </div>
+
+    <!---- Accident Details---->
+
+    <div class="panel panel-default" id = "accident_info">
+        <div class="panel-heading"><h4>Accident Details</h4></div>
+        <div class="panel-body">
+            <b>Description: </b> <?php echo $description['value']; ?>
+            <p></p>
+            <b>Severity:</b> <?php echo severity_scale($severity); ?>
+            <p></p>
+            <b>Root Cause:</b> <?php echo $root['value']; ?>
+            <p></p>
+            <b>Prevention:</b> <?php echo $prevention['value']; ?>
+            <p></p>
+        </div>
+    </div>
+
+    <div class =" panel panel-default" id="photos">
+        <div class="panel-heading"><h4>Accident Photos</h4></div>
+        <div class="panel-body">
+
+
+<?php
+$params = array($details->id);
+
+$this->load->library('gallerybuilder', $params);
+?>
+
+
+
+            <!-----------------Demo---------------------->
+        </div>
+    </div>
+    
+<?php echo form_open("accidents/comment", array("class" => "form-horizontal formcs", "role" => "form","encytype" =>"")); ?>
+    
+    
+    
+    
+    
+    
+    <?php echo form_button(array("type" => "submit", "class" => "btn btn-success", "content" => '<span class="glyphicon glyphicon-plus"></span> Add Report')); ?>
 <?php echo form_close(); ?>
