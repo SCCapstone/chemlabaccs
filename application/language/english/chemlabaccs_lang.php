5c759c6930818e65ee3bef1c5f742ad1f87959c5
timciep <cieplows@email.sc.edu>
2014-02-23 16:23:56 -0500

    new stored string

--- application/language/english/chemlabaccs_lang.php
+++ application/language/english/chemlabaccs_lang.php
@@ -6,3 +6,4 @@ if (!defined('BASEPATH'))
 $lang['cla_f_description'] = "A brief description of the accident";
 $lang['cla_f_root'] = "The root cause of the accident";
 $lang['cla_f_prevention'] = "Ways to prevent the accident in the future";
+$lang['cla_f_join'] = "Enter the Section ID Number and Password given to you by your instructor.";
