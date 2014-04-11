295ccab7d5ac8e8585044ce2211b8a17ca14a19e
dangerdave10@gmail.com <alexan84@email.sc.edu>
2014-04-11 19:36:06 -0400

    Added search by keyword input box on nav bar

--- application/views/users/navbar-signed-in.php
+++ application/views/users/navbar-signed-in.php
@@ -4,6 +4,12 @@ if (!defined('BASEPATH'))
 ?>
 
 <ul class="nav navbar-nav navbar-right">
+    <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search by keyword">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Signed in as <strong><?php echo CI()->auth->get_user_name(); ?></strong> <b class="caret"></b></a>
         <ul class="dropdown-menu">
@@ -11,4 +17,5 @@ if (!defined('BASEPATH'))
             <li><?php echo anchor('dashboard/switch_theme', '<span class="glyphicon glyphicon-transfer"></span> Switch theme') ?></li>
         </ul>
     </li>
+     
 </ul>
