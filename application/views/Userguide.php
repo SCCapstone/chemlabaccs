4d56b4bebe33414632dd3315863cf8ee8122526c
Allenda <allenda@email.sc.edu>
2014-04-18 12:15:24 -0400

    Added userguide.php

---
+++ application/views/Userguide.php
@@ -0,0 +1,58 @@
+<?php
+
+/* 
+ * User Guide File.
+ * Author: David A. Allen
+ * 
+ */
+
+<h2>User Guide <small> Lab Accident Reporting System</small></h2>
+    <dl class="dl-horizontal">
+    <dd>The Lab Accident Reporting System (LARS) is a web application whose purpose is to allow users to create accident reports that may occur in various types of laboratories.  Whether you are in Chemistry or Physics lab or even a Mechanical Engineering lab, LARS makes it simple to document accidents for the professor and class to view so that these types of accident can be avoided in the future. 
+    Professors or lab Teaching Assistants have the ability to create their own sections for the students to join who are currently taking their labs.  Not only can the users view lab reports from their sections, but LARS also offers a correlated data chart of all lab accidents submitted so that students and teachers alike can assess which accidents are most frequent and plan accordingly.  
+    The following is the user’s guide to operating LARS: </dd>
+    <br/>
+    <dt>Getting Started </dt>
+    <dd>In order to have access to the LARS features, a user must own an account.  If an account is owned, enter the username and password in the respective boxes and click “Sign in”.
+    If an account is not owned, the user must create an account by clicking the “Register” button on the bottom of the page.  A page will then appear titled “Register as new User”.  The user must fill out the boxes labeled: “Email”, “Password”, and “Password Confirmation”.  
+    If the user is an administrator (setting up a section or class for others to join), then that user would click the “Administrator” button.  If the user is wishing to join a section (student), then he/she would click the “User” button.  Click “Register” once all data fields are filled in.  If this has been completed successfully, an email will be sent to the user’s given address providing their username and password.
+    The user will be directed to a new page that informs him/her of the status of the account.  If an error has occurred, follow the instructions in the message box to correct the error.  If the account has been successfully created, the user will be prompted to log-in using their chosen username (email) and password combination.  Click “Sign In”.
+    The home page consists of a “Reporting” box, “Your Account” box, and “Your Sections” box. </dd>
+    <br/>
+    <dt>Creating a Class Section </dt>
+    <dd>If the user is an admin, they should first create a section by clicking the “Create a Section” button located in the “Your Account” box.  Once clicked, the user is directed to the “Create a Section” page.  This page consists of 6 criteria for a section.  The user is responsible for creating a Section Name and a Section Password for your students.  It is recommended that the Section Name is easily recognized by the students in this section.  Fill in the term (Fall, Spring, Summer 1, Summer 2, Summer 3, or Other) with the correct term.  Choose the year for the class, Enter the building name where the laboratory is where the class meets, and enter the room number of the lab. Once all criteria is filled in correctly, click “Create Section”.  Pending the section was created successfully, an email will be sent to the admin providing a Section ID # and the section password.  You can also log out using the “Your Account” section.  
+    The user will now notice that under the “Your Sections” box, Name, ID, and Password for the created section are displayed.  This is for reference. </dd>
+    <br/>
+    <dt>Joining a Section </dt>
+    <dd>If you are not an administrator, you will want to join a pre-existing section set up by your professor or teaching assistant.  In the “Your Account” box, click on “Join Section”.  The site will navigate to a page titled “Join a Section”.  You will be prompted to provide a Section ID # and a Section Password.  These must be provided by your professor or teaching assistant.  Click “Join!”.  If information is correct you will be routed to the home page with the three boxes.  </dd>
+    <br/>
+    <dt>Adding an Accident Report </dt>
+    <dd>To add an accident report, make sure you are on the home page by clicking “LARS” in the top left of the page.  Find “Add Accident Report” under the “Reporting” box and click “Go”.  
+    The user will now see the “Add Accident Report” blank form.  First click the drop-down list and click on the section associated with the accident.  Note that if you are not in any sections, this list will be blank.  Next choose the date when the accident occurred.  The date can either be entered manually or the user can click on the calendar to the right of the box and select the date that way.  To enter the time, click on the first set of dashes and type in the hour.  For example if the time is 9:30 AM, the user can simply type 9 and it will be displayed as “09”.  Enter the minutes next using the same techniques, then choose AM or PM.  If you do not wish to type the time manually, select which section you are applying (hours, minutes, AM/PM) and use the arrows to the far right to choose the number or value.  
+    The next box is for a description of the accident itself.  How much the user types is preferential to that user.  Following that, choose a severity of the accident.  If it is a low severity (such as broken glass, etc.) choose “Low”.  For more severe accidents (spilt chemicals, injuries, etc.) choose a higher severity.  Next a Root cause is prompted.  This is where the user can write a brief description of how the accident occurred.  After that, a prevention box is provided to allow the user to write up a way to prevent the accident in the future.  
+    Underneath the boxes, there is an option to add photos of the accident to the Accident Report.  If the user wishes to add photos, click “Add Photos” and select the photos you wish to add on your computer or phone.
+    Once the user is satisfied with their input, click “Add Report” to submit the report.
+    Another way to access this feature is to click on the accidents drop down menu and select “Add”.  </dd>
+    <br/>
+    <dt>Searching for an Accident Report/Editing an Accident Report </dt>
+    <dd>To search for a pre-existing accident report click on “Go” under “Search Accident Reports”.  The Search Accidents Page will pull up.  Enter the section corresponding with the accident along with the start and end dates (clicking on the box will pull up a calendar).  Next enter the start and end time coordinating with the accident submission. Finally click on the severities related to the accident.  Note, if the user does not know a certain field or fields, he/she may leave these blank. Multiple severities may be chosen as well.  Leaving the entire page blank will return every report.  
+    Once a search has been submitted, the corresponding results will be listed vertically.  To view a report, click on “Details” on the far right.  Doing so will take you to the details of the accident report for viewing purposes only.  However at the bottom there is a comment box so that users can add comments pertaining to the accident.  If the user finds that some information is incorrect on the report, there is an edit button on the top right to bring up the original form to be edited.  
+    Another way to access this feature is to click on the accidents drop down menu and select “Search”. </dd>
+    <br/>
+    <dt>Viewing Your Own Accident Reports </dt>
+    <dd>To view all of the accident reports that the user has submitted, click on “My Accident Reports” under the “Reporting” box.  Once clicked, the user will be directed to the “My Accident Reports” page where all of their reports are listed.  One can scroll through to look for a report in particular.  To open the report click on the “Details” button associated with that report (located on the far right of the specified report).  Doing so will take you to the details of the accident report for viewing purposes only.  However at the bottom there is a comment box so that users can add comments pertaining to the accident.  If the user finds that some information is incorrect on the report, there is an edit button on the top right to bring up the original form to be edited. 
+    The user can also access his/her reports by clicking the Reporting drop down menu at the very top of the page and selecting “My Reports”.</dd>
+    <br/>
+    <dt>Exporting Report Data to a CSV File </dt>
+    <dd>To export the user’s own accident reports to a .CSV file (ex. Excel), Click on the Reporting drop down menu then click on “Export to CSV”.  A .CSV file will open with the default .CSV program for the user’s computer or phone for use.   </dd>
+    <br/>
+    <dt>Questions? </dt>
+    <dd>Feel free to contact any of us via email.
+    <ul>
+    <li>Tim Cieplowski @ cieplows@email.sc.edu </li>
+    <li>David Allen @ allenda@email.sc.edu </li>
+    <li>Davis Alexander @ alexan84@email.sc.edu </li>
+    <li>Demetrious Cooper @ cooperd2@email.sc.edu </li></dd>
+    </ul>
+    <br/>
+
