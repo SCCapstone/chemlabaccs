ccc5d525355b7d5febe3be9d909b509dee2698c3
timciep <cieplows@email.sc.edu>
2014-04-26 23:21:08 -0400

    link for about page now points to correct place

--- application/views/users/navbar-signed-in.php
+++ application/views/users/navbar-signed-in.php
@@ -9,6 +9,7 @@ if (!defined('BASEPATH'))
         <ul class="dropdown-menu">
             <li><?php echo anchor('dashboard/switch_theme', '<span class="glyphicon glyphicon-transfer"></span> Switch theme') ?></li>
             <li><?php echo anchor('dashboard/help', '<span class="glyphicon glyphicon-book"></span> Help!') ?></li>
+            <li><?php echo anchor('dashboard/about', '<span class="glyphicon glyphicon-book"></span> About') ?></li>
             <li><?php echo anchor('users/signout', '<span class="glyphicon glyphicon-user"></span> Sign out') ?></li>
         </ul>
     </li>
